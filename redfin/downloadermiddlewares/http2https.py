d4427901fbac905035b8ee369dacb2d66c2c8be1
jerryan9999 <jerry.an@rentx.io>
2017-05-24 09:35:37 +0800

    fix https

--- redfin/downloadermiddlewares/http2https.py
+++ redfin/downloadermiddlewares/http2https.py
@@ -2,8 +2,6 @@
 class HttpschangeMiddleware(object):
 
   def process_request(self, request, spider):
-    if request.url.startswith("http://"):
-      https_url = request.url.replace("http://","https://")
-      http_url_new = https_url.replace('status=9','status=-1').replace('status=1','status=-1').replace('num_homes=350','num_homes=35000')
-      r= request.replace(url=https_url_new)
-      return  r
+    https_new = request.url.replace("http://","https://").replace('status=9','status=-1').replace('status=1','status=-1').replace('num_homes=350','num_homes=35000')
+    r= request.replace(url=https_new)
+    return  r
--- redfin/settings.py
+++ redfin/settings.py
@@ -27,7 +27,7 @@ CONCURRENT_REQUESTS = 48
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 48
 CONCURRENT_REQUESTS_PER_IP = 1
