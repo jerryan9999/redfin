75dee7c671787c53b5300e089a8349af725344ee
jerryan9999 <jerry.an@rentx.io>
2017-05-24 09:50:08 +0800

    https2

--- redfin/downloadermiddlewares/http2https.py
+++ redfin/downloadermiddlewares/http2https.py
@@ -4,4 +4,3 @@ class HttpschangeMiddleware(object):
   def process_request(self, request, spider):
     https_new = request.url.replace("http://","https://").replace('status=9','status=-1').replace('status=1','status=-1').replace('num_homes=350','num_homes=35000')
     r= request.replace(url=https_new)
-    return  r
